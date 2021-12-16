76cedbf36250a64c38468b5af4ed6a43801669c5
opilane <opilane@HP-CZC6297BB7>
2021-12-16 15:30:54 +0200

    ˇ112

--- Funktsioonid/Funktsioonid.py
+++ Funktsioonid/Funktsioonid.py
@@ -1,4 +1,5 @@
 from Module import*
+from math import*
 
 #S1=Kolmnurga_pindala(4,6)
 #print(S1)
@@ -9,6 +10,23 @@ from Module import*
 #S3=Kolmnurga_pindala(-77,6)
 #print(S3)
 #.....................Практическая работа номер 1 
-S6=arithmetic(float(input("enter num1: ")),float(input("enter num2: ")),str(input("enter simbol: ")))
-print(S6)
+#S6=arithmetic(float(input("enter num1: ")),float(input("enter num2: ")),str(input("enter simbol: ")))
+#print(S6)
 
+# ___________________________________ 3
+#a=float(input("Enter numb: "))
+#S1=ruud(a)
+#print (S1)
+#______________________________________ 4 
+#a=int(input("sistage arv:  "))
+#S1=season(a)
+#print(S1)
+#______________________________________ 5
+#a=float(input("enter sum: "))
+#b=int(input("enter years: "))
+#s1=bank(a,b)
+#print(s1)
+#______________________________________ 6 
+a=int(input("sisestage arv:  "))
+s1=is_prime(a)
+print(s1)
--- Funktsioonid/Module.py
+++ Funktsioonid/Module.py
@@ -1,3 +1,4 @@
+from math import*
 def Kolmnurga_pindala(külg:float, kõrgus:float):
     """leiab kolmnurga pindala. On antud kõrgus ja külje pikkus. Tagastab Sfloat formaadis
     :param float külg: Kolmnurga külje pikkus
@@ -32,3 +33,60 @@ def arithmetic(num1:float, num2:float, argument:str):
     else:
         vastus="Неизвестная операция"
     return vastus
+def ruud (a:float):
+    """
+    :rtype:  float, float, float
+    :param float a: ruudu külg
+    """
+    while 1: 
+        d=a*sqrt(2)
+        P=a*4
+        S=a*a
+    return P,S,d
+def season (kuu:int)->str:
+    """
+    Võtame kuu umber ja tagastame tekst: Talv, Kevad, Suvi või Sügis.
+    """
+    while 1:
+        if kuu>=1 and kuu<=12:
+           if kuu in [1,2,12]:
+              s="Talv"
+           elif kuu in [3,4,5]:
+              s="Kevad"
+           elif kuu in [6,7,8]:
+              s="Suvi"
+           else:
+              s="Sügis"
+           break
+        else:
+           kuu=int(input("vale kuu, sissestage usesti: "))
+    return s
+def bank(a:float, years:int)->float:
+    """
+    Рассчитываем сумму с годовыми от нашего вклада 
+    :param float a: вклад
+    :param int years: срок в годах  
+    :rtype: a float 
+    """
+    if a >=0:
+        for i in range (years):
+            a*=1.1
+    else:
+        a="Сумма не может быть отрицательной!"
+    return a
+def is_prime(a:int)->bool:
+    """
+    peate leidma algarvud vahemikus 0 kuni 1000
+    :param a int: arv 
+    :rtype:  a int 
+    """
+    t=0 
+    for i in range (1, arv+1):
+        if arv%i==0: t+=1
+    if t==2:
+        t=True
+    else:
+        t=False
+    return t 
+
+
